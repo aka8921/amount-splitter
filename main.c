eaecea400ee1e88d84abbd9adb81a40baf2a8822
anugeeth <anugeeth2@gmail.com>
2020-04-22 19:27:04 +0530

    minor fixes

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include <stdio.h>
 #include <string.h>
 
@@ -14,6 +16,7 @@ int p_count = 0;
 
 void person()
 {
+    printf("\033[0;31m");
     // add new person
     if (p_count < 6)
     {
@@ -27,6 +30,7 @@ void person()
     {
         printf("Limit exceeded");
     }
+    printf("\033[0m");
 }
 
 void expenses()
@@ -42,7 +46,6 @@ void expenses()
         printf("Name of the person\t");
         scanf("%s", name);
         fgets(name, sizeof(name), stdin);
-
         while (count <= p_count)
         {
             if (strcmp(p[count].name, name) == 0)
@@ -57,23 +60,86 @@ void expenses()
             }
         }
         if (!status)
-        {
             printf("Person not found !!!!");
+        else
+        {
+            printf("%s%s", "Enter expenses of  ", p[count].name);
+            for (int i = 0; i < 6; i++)
+            {
+                printf("%s%s", "Enter expense of activity ", p[count].name);
+                scanf("%d", &p[count].expenses[i]);
+            }
         }
     }
-    p[count].expenses[]
 }
+
+// char activities()
+// {
+//     char *act[] = {"activity 1", "activity 2", "activity 3", "activity 4", "activity 5"};
+//     return (char *) act;
+// }
 
 void calculate()
 {
     // calculates and split up expenses between each person
+    int total_day_expenses = 0;
+    int len = sizeof(p);
+    for (int i = 0; i <= len; i++)
+    {
+        for (int j = 0; j <= 5; j++)
+        {
+            total_day_expenses = total_day_expenses + p[i].expenses[j];
+        }
+    }
+    printf("%d", total_day_expenses);
 }
 
 int main()
 {
+    int option;
+    // do
+    // {
+    //     printf("\n 1. Add Person\n 2. Add Expenses \n 3. View Split Up\n 4. Exit\n\n Select one to continue:\t");
+    //     scanf("%d", &option);
+    //     // option = getchar();
+    //     switch (option)
+    //     {
+    //     case 1:
+    //         person();
+    //         break;
+    //     case 2:
+    //         expenses();
+    //         break;
+    //     case 3:
+    //         calculate();
+    //         break;
+    //     case 4:
+    //         // exit(0);
+    //         break;
+    //     default:
+    //         printf("Invalid Option");
+    //     }
+    //     printf("What next ?");
+    // } while (option != 4);
 
-    person();
-    person();
-    expenses();
+    do
+    {
+        printf("\n 1. Add Person\n 2. Add Expenses \n 3. View Split Up\n 4. Exit\n\n Select one to continue:\t");
+        scanf("%d", &option);
+        // option = getchar();
+        if (option == 1)
+            person();
+        else if (option == 2)
+            expenses();
+        else if (option == 3)
+            calculate();
+        else if (option == 4)
+            exit(0);
+
+        else
+            printf("Invalid Option");
+        printf("What next ?");
+    } while (option != 4);
+
     return 0;
 }
